f4a8e7bc2309668ed67edccd679824f6469c9f38
unidesk <vinicius.nogueira@unidesk.com.br>
2022-11-25 12:30:54 -0300

    Lista interativa com separação de responsabilidades

--- app/src/main/java/projetkotlin/com/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/projetkotlin/com/dynamiccontentexample/MainActivity.kt
@@ -27,23 +27,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val greetingListState = remember {
+        mutableStateListOf<String>("John", "Amanda")
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GreetingList()
+        GreetingList(greetingListState) {
+            greetingListState.add("Miguel")
+        }
     }
 }
 
 @Composable
-fun GreetingList() {
-    val greetingListState = remember { mutableStateListOf<String>("John", "Amanda") }
-    for (name in greetingListState) {
+fun GreetingList(namesList: List<String>, buttonClick: () -> Unit) {
+    for (name in namesList) {
         Greeting(name = name)
     }
 
-    Button(onClick = { greetingListState.add("Miguel") }) {
+    Button(onClick = buttonClick) {
         Text(text = "Add name")
     }
 }
